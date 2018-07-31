ff6cc366c98e42d578bde9b27c71d2877130939b
I-Iege <hegedus.attila2@gmail.com>
2018-07-31 12:09:20 +0200

    UnitTest added

---
+++ LegendsOfCodeAndMagic/src/CCGBotUnitTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CCG;
+using System.Collections.Generic;
+
+namespace CCGBotUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_GetMostExpensiveCard()
+        {
+            List<Card> cards = new List<Card>()
+           {
+               new Card(0, 0, 0, 0, 5, 0, 0, string.Empty, 0, 0, 0),
+               new Card(1, 0, 0, 0, 4, 0, 0, string.Empty, 0, 0, 0),
+               new Card(2, 0, 0, 0, 3, 0, 0, string.Empty, 0, 0, 0),
+               new Card(2, 0, 3, 0, 2, 0, 0, string.Empty, 0, 0, 0),
+               new Card(2, 0, 1, 0, 3, 0, 0, string.Empty, 0, 0, 0),
+           };
+           Assert.AreEqual(cards[0], Util.GetMostExpensiveCard(5, 0, 0, cards));
+           Assert.AreEqual(cards[3], Util.GetMostExpensiveCard(5, 6, 0, cards));
+           Assert.AreEqual(cards[1], Util.GetMostExpensiveCard(4, 5, 0, cards));
+           Assert.AreEqual(cards[4], Util.GetMostExpensiveCard(5, 6, 0, cards));
+        }
+
+        [TestMethod]
+        public void Test_GetBestCard()
+        {
+
+        }
+    }
+}
